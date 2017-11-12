1f95d96ed1545ad220e4a5280f099313daf27561
Storm <wenyejie@foxmail.com>
2017-11-12 11:57:35 +0800

    优化项目, 载入polyfill

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 // import "babel-polyfill"
-import './styles'
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import reportError from './cores/reportError'
+import './styles'
 // import components from './components'
 
 // Vue.use(components)
