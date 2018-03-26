8c07af6c0ec50689e10730a1e00ea1ed8bcd17e9
Storm <wenyejie@foxmail.com>
2018-03-26 16:55:34 +0800

    tag开发

---
+++ src/components/tag/index.js
@@ -0,0 +1,8 @@
+/**
+ * tag
+ *
+ * @author: Storm
+ * @date: 2018/03/26
+ */
+
+export default resolve => require(['./tag.vue'], resolve);
---
+++ src/components/tag/tag.vue
@@ -0,0 +1,21 @@
+<!--
+ - tag
+ -
+ - @author: Storm
+ - @date: 2018/03/26
+ -->
+<template>
+  <span class="s-tag"></span>
+</template>
+
+<script>
+  export default {
+    name: 'sTag'
+  }
+</script>
+
+<style lang="scss">
+  .s-tag {
+
+  }
+</style>
