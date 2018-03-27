1d75cd1fec186a428e54d4f6f392a75eb0b336ec
Storm <wenyejie@foxmail.com>
2018-03-27 14:57:16 +0800

    tag开发

---
+++ src/components/label/index.js
@@ -0,0 +1,8 @@
+/**
+ * label
+ *
+ * @author: Storm
+ * @date: 2018/03/26
+ */
+
+export default resolve => require(['./label.vue'], resolve)
---
+++ src/components/label/label.vue
@@ -0,0 +1,21 @@
+<!--
+ - label
+ -
+ - @author: Storm
+ - @date: 2018/03/26
+ -->
+<template>
+  <span class="s-label"></span>
+</template>
+
+<script>
+  export default {
+    name: 'sLabel'
+  }
+</script>
+
+<style lang="scss">
+  .s-label {
+
+  }
+</style>
