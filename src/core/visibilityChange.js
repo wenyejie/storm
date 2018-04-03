998b76e8efb8a2f3765b7d564f2a86fea8866beb
Storm <wenyejie@foxmail.com>
2018-04-03 14:10:58 +0800

    判断页面可见性方法调整

--- src/core/visibilityChange.js
+++ src/core/visibilityChange.js
@@ -2,19 +2,28 @@
  * 判断页面是否是可见状态
  * https://caniuse.com/#search=visibilityState
  * https://caniuse.com/#search=visibilitychange
- *
- * @param callback 回调
  * @author: Storm
  * @date: 2018/04/03
- * @return {null}
  */
+
+/**
+ * 判断页面是否是可见状态
+ * https://caniuse.com/#search=visibilityState
+ * https://caniuse.com/#search=visibilitychange
+ *
+ * @author: Storm
+ * @date: 2018/04/03
+ */
+
+// 属性名称
+const attrName = ['visibilityState', 'webkitVisibilityState'].find(item => `${item}` in document)
+
+// 事件名称
+const eventName = attrName.replace(/state/i, 'change')
+
 export default (callback) => {
 
-  const attrName = ['visibilityState', 'webkitVisibilityState'].find(item => `${item}` in document)
-
   if (!attrName) return null
-
-  const eventName = attrName.replace(/state/i, 'change')
 
   document.addEventListener(eventName, () => {
 
