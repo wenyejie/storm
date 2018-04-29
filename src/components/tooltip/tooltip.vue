349c6e62faee1d9fee9d248f43e191a5884b8e47
Storm <wenyejie@foxmail.com>
2018-04-29 11:48:09 +0800

    新增tooltip组件

---
+++ src/components/tooltip/tooltip.vue
@@ -0,0 +1,21 @@
+<!--
+ - tooltip
+ -
+ - @author: Storm
+ - @date: 2018/04/29
+ -->
+<template>
+  <div class="s-tooltip"></div>
+</template>
+
+<script>
+  export default {
+    name: 'cTooltip'
+  }
+</script>
+
+<style lang="scss">
+  .s-tooltip {
+
+  }
+</style>
---
+++ src/view/components/tooltip.vue
@@ -0,0 +1,24 @@
+<!--
+ - tooltip
+ -
+ - @author: Storm
+ - @date: 2018/04/29
+ -->
+<template>
+  <div class="page page-tooltip">
+    <s-panel title="Default 默认">
+      
+    </s-panel>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vTooltip',
+    title: 'tooltip'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
