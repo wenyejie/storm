804a6b1540368bd1b73b2328aea00d1f7cfc8004
Storm <wenyejie@foxmail.com>
2018-07-17 10:38:38 +0800

    温度转换

---
+++ src/utils/celsius2fahrenheit.js
@@ -0,0 +1,8 @@
+/**
+ * 摄氏度转华氏度
+ *
+ * @author: Storm
+ * @date: 2018/07/17
+ */
+
+export default fahrenheit => (fahrenheit - 32) * 5 / 9
---
+++ src/utils/fahrenheit2celsius.js
@@ -0,0 +1,8 @@
+/**
+ * 华氏度转摄氏度
+ *
+ * @author: Storm
+ * @date: 2018/07/17
+ */
+
+export default celsius => (celsius * 9 / 5) + 32
